bc880f76ed61753619bbd9bd76ce05bc55279009
535619860@qq.com <535619860@qq.com>
2017-01-05 23:56:46 +0800

    all files

---
+++ entry.js
@@ -0,0 +1,12 @@
+	
+/*** entry.js ***/
+document.getElementById('app').innerHTML="这是我第一个打包成功的程序";
+require("./first.js");
+require("./style.css");
+var Vue =require("vue");
+    new Vue({
+        el: "body",
+        data: {
+            message: "hello vue.js"
+        }
+    });
